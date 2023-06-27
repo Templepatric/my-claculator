9a648b8bc2cc00e38339c219503f7fd345d92694
goodnesskanu <goodnesskanu550@gmail.com>
2023-06-27 01:50:26 -0700

    my claculator

---
+++ app.js
@@ -0,0 +1,20 @@
+// const job2 = {
+//     role: "Software"
+//     salary: 200.000
+//     application: "pmt.ng /career/ Software",
+//     isRemote:false
+    
+// }
+
+// //show answer of the role
+
+// class.job2{
+//     constructor(role,salary,application,isRemote){
+//         this.role = role;
+//         this.salary = salary,
+//         this.application = application;
+//         this.isRemote = isRemote;
+//     }
+
+//     console.log(job2)
+// }
